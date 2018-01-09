1b694eb5fc17ff88f3e034cdf90ba6c2cd3c13c3
Ravi Teja Tatipamula <teja2021@gmail.com>
2018-01-08 22:19:12 -0800

    Leet Code - 236
    Least Common Ancestor in a Binary Tree

---
+++ LCA_in_BT.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None or p is None or q is None:
+            return None
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if left and right:
+            return root
+        elif left is None and right is None:
+                return None
+        return left if left else right
