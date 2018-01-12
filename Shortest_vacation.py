40266869bfb98d966508f320e2162ed7d8cab683
Ravi Teja Tatipamula <teja2021@gmail.com>
2018-01-12 01:13:45 -0800

    Shortest possible vacation.

---
+++ Shortest_vacation.py
@@ -0,0 +1,23 @@
+def solution(A):
+    for i in range(len(A)):
+        if A[i] in A[i+1:]:
+            pass
+        else:
+            start = i
+            break
+    if i == len(A)-1:
+        return 1
+    
+        
+    for i in range(len(A)-1,1,-1):
+        print(A[start:i])
+        if A[i] in A[start:i]:
+          print("Yes")  
+          pass
+        else:   
+            end  = i
+            break
+    print(end,start)
+    return  (end -start + 1)
+    
+print(solution([1,2]))
