30662f604507be6341bf94261bdde57c59076bee
Ravi Teja Tatipamula <teja2021@gmail.com>
2018-10-22 14:40:35 -0700

    Intersection of two arrays

---
+++ Leet-Code-350.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        d1 = {}
+        d2 = {}
+        for i in nums1:
+            if i in d1:
+                d1[i] += 1
+            else:
+                d1[i] = 1
+        for i in nums2:
+            if i in d2:
+                d2[i] += 1
+            else:
+                d2[i] = 1
+        res = []
+        for key in d1:
+            if key in d2:
+                times = min(d1[key], d2[key])
+                for _ in range(times):
+                    res.append(key)
+        return res
+        
